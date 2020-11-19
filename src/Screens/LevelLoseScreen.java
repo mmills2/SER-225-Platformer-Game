99f100485a2f22c630e350aea2b2d45a321c62d9
Danny <danchristie991@gmail.com>
2020-11-19 15:32:15 -0500

    Added the lives mechanic, the player now resets current level on death unless lives are at 1 remaining, then it gives a death screen

--- src/Screens/LevelLoseScreen.java
+++ src/Screens/LevelLoseScreen.java
@@ -36,6 +36,7 @@ public class LevelLoseScreen extends Screen {
         // if space is pressed, reset level. if escape is pressed, go back to main menu
         if (Keyboard.isKeyDown(Key.SPACE)) {
             playLevelScreen.restartGame();
+            PlayLevelScreen.setLives(3);
         } else if (Keyboard.isKeyDown(Key.ESC)) {
             playLevelScreen.goBackToMenu();
         }
